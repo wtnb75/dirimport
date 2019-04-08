3ffdd8f0f65bd9fa19893badb091ca1211395f58
Takashi WATANABE <wtnb75@gmail.com>
2019-04-09 01:07:52 +0900

    setup.py

--- dirimport/cli.py
+++ dirimport/cli.py
@@ -2,6 +2,8 @@ import os
 import click
 from . import gen
 
+VERSION = "0.0.1"
+
 
 @click.group(invoke_without_command=True)
 @click.pass_context
@@ -10,19 +12,19 @@ def cli(ctx):
         print(ctx.get_help())
 
 
-@cli.command()
+@cli.command("diff")
 @click.argument("path", type=click.Path(exists=True, dir_okay=True, file_okay=False))
 @click.option("--filename", default="__init__.py")
-def diff(path, filename):
+def diffcmd(path, filename):
     data = gen.dig(path)
     click.echo(
         "\n".join(map(lambda f: f.rstrip(), gen.diff(data, path, filename))))
 
 
-@cli.command()
+@cli.command("generate")
 @click.argument("path", type=click.Path(exists=True, dir_okay=True, file_okay=False))
 @click.option("--filename", default="__init__.py")
-def generate(path, filename):
+def generatecmd(path, filename):
     data = gen.dig(path)
     gen.generate(data, path, filename)
 
@@ -30,7 +32,7 @@ def generate(path, filename):
 @cli.command("eval")
 @click.argument("path", type=click.Path(exists=True, dir_okay=True, file_okay=False))
 @click.argument("expr")
-def showvalue(path, expr):
+def evalcmd(path, expr):
     bn = os.path.basename(path)
     globals()[bn] = gen.importall(path)
     click.echo(eval(expr))
